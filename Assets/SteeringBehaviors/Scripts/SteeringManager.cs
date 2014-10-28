d2a49aa1e44cf463190308c97d9f1ceb7dfd860d
Sam Bloomberg <sam@redxdev.com>
2014-10-28 16:19:14 -0400

    setup unity project for vcs

---
+++ Assets/SteeringBehaviors/Scripts/SteeringManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringManager : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
