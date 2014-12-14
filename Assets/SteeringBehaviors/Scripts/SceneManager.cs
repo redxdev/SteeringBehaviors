6693d3de0992ef8dab857285e24a193fb63dee07
Sam Bloomberg <sam@redxdev.com>
2014-12-13 20:14:01 -0500

    added new scenes and demos

---
+++ Assets/SteeringBehaviors/Scripts/SceneManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+    public void ChangeScene(int scene)
+    {
+        Application.LoadLevel(scene);
+        Application.ExternalCall("sceneLoaded", "ok");
+    }
+}
